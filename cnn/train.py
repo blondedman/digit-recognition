1276c22ed0597c706f008e1fa6d70a67e87432ab
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-08-18 20:53:56 +0530

    Update train.py

--- cnn/train.py
+++ cnn/train.py
@@ -18,3 +18,12 @@ import numpy as np
 import argparse
 import torch
 import time
+
+INIT_LR = 1e-3
+BATCH_SIZE = 64
+EPOCHS = 10
+
+TRAIN_SPLIT = 0.75
+VALID_SPLIT = 1 - TRAIN_SPLIT
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
